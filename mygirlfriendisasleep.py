fb3798e1fcb55c4a06e55d9c62999927e67614f8
KTheMan <k@knnygrdn.com>
2023-09-24 06:23:51 -0700

    Change Windows dependency installation improves "launching" as a byproduct

--- mygirlfriendisasleep.py
+++ mygirlfriendisasleep.py
@@ -6,24 +6,101 @@ import platform
 import subprocess
 import time
 import datetime
+import requests
 
 def install_dependency(dep_name):
     if dep_name == "nircmd":
         try:
-            subprocess.run(["winget", "install", "nircmd"], check=True)
+            subprocess.run(["winget", "install", "gerardog.gsudo"])
+            refresh_environment()
+            subprocess.run(["gsudo", "winget", "install", "nircmd"], check=True)
+            refresh_environment()
         except subprocess.CalledProcessError:
             print("Failed to install nircmd using winget. Please manually download nircmd from https://www.nirsoft.net/utils/nircmd.html and place it in a directory in your PATH or the current directory.")
     elif dep_name == "caffeinate":
         print("caffeinate should be preinstalled on macOS. Please ensure it's available.")
     elif dep_name == "xset":
-        subprocess.run(["sudo", "apt-get", "install", "x11-xserver-utils"])
+        cmd = ["sudo", "apt-get", "install", "x11-xserver-utils"] if not is_elevated() else ["apt-get", "install", "x11-xserver-utils"]
+        subprocess.run(cmd)
 
 def check_dependency(dep_name):
     try:
-        subprocess.run([dep_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        return True
-    except FileNotFoundError:
+        if dep_name == "nircmd":
+            result = subprocess.run(["winget", "list"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            if "nircmd" in result.stdout:
+                return True
+
+        elif dep_name == "xset":
+            result = subprocess.run(["dpkg", "-l"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            if "x11-xserver-utils" in result.stdout:
+                return True
+
+        else:
+            subprocess.run([dep_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            return True
+
+    except Exception as e:
+        print(f"Error checking dependency {dep_name}: {e}")
         return False
+
+    return False
+
+import os
+import subprocess
+
+def refresh_environment():
+    ps_script = '''
+    $userName = $env:USERNAME
+    $architecture = $env:PROCESSOR_ARCHITECTURE
+    $psModulePath = $env:PSModulePath
+
+    $ScopeList = 'Process', 'Machine'
+    if ($userName -notin 'SYSTEM', "${env:COMPUTERNAME}`$") {
+        $ScopeList += 'User'
+    }
+    foreach ($Scope in $ScopeList) {
+        Get-ChildItem -Path "Env:" | ForEach-Object {
+            $envVarName = $_.Name
+            Set-Item "Env:$envVarName" -Value ([System.Environment]::GetEnvironmentVariable($envVarName, $Scope))
+        }
+    }
+
+    $paths = 'Machine', 'User' | ForEach-Object {
+        ([System.Environment]::GetEnvironmentVariable('PATH', $_)) -split ';'
+    } | Select-Object -Unique
+    $Env:PATH = $paths -join ';'
+
+    $env:PSModulePath = $psModulePath
+
+    if ($userName) { $env:USERNAME = $userName; }
+    if ($architecture) { $env:PROCESSOR_ARCHITECTURE = $architecture; }
+    '''
+
+    # Execute the PowerShell script
+    result = subprocess.run(['powershell', '-Command', ps_script], capture_output=True, text=True)
+    
+    if result.returncode != 0:
+        print(f"Error occurred: {result.stderr}")
+    else:
+        print("Environment updated in PowerShell session!")
+
+    # Update PATH in the current Python environment
+    fetch_updated_path()
+
+def fetch_updated_path():
+    # Fetch the updated PATH using PowerShell
+    cmd = 'powershell -Command "[System.Environment]::GetEnvironmentVariable(\'PATH\', \'Machine\')"'
+    result = subprocess.run(cmd, capture_output=True, text=True, shell=True)
+    
+    # Split both old PATH and new PATH into lists
+    old_path = set(os.environ['PATH'].split(';'))
+    new_path = set(result.stdout.strip().split(';'))
+    
+    # Update the old PATH with entries from the new PATH
+    combined_path = old_path.union(new_path)
+    os.environ['PATH'] = ';'.join(combined_path)
+
+    print("Environment updated in Python!")
 
 def turn_off_display():
     sys_platform = platform.system()
@@ -62,7 +139,7 @@ def main():
     turn_on_cmd = "on" in sys.argv
 
     if len(sys.argv) < 2:
-        print("Usage: mygirlfriendisasleep [off/on/shesasleepuntil HH:MM]")
+        print("Usage: mygirlfriendisasleep [off/on] [off arguments: --shesasleepuntil HH:MM] [on arguments: --force]")
         return
 
     if turn_on_cmd and not force_turn_on and set_sleep_time:
